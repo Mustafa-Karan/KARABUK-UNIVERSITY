8b56b72571c687fd210f13518b8c2204cec1afa4
Mustafa Karan <115796509+Mustafa-Karan@users.noreply.github.com>
2023-03-04 22:59:18 +0300

    Update CRAPS-GAME.c

--- FIRST-YEAR/SPRING-TERM-PROGRAMMING-LANGUAGES-2/HOME-WORKS/CRAPS-GAME.c
+++ FIRST-YEAR/SPRING-TERM-PROGRAMMING-LANGUAGES-2/HOME-WORKS/CRAPS-GAME.c
@@ -5,20 +5,22 @@
 int main(){
 	srand(time(NULL));
 	int die1,die2,sum1=0,sum2=0,choice;
+	printf("**** WELCOME THE CRAPS GAME ****\n\n ");
 	printf("TO PLAY ENTER 1 TO EXIT ENTER 0\n");
 	scanf("%d",&choice);
-	while(1){
 		switch(choice){
 			case 1:
 				die1=rand()%6+1;
 				die2=rand()%6+1;
 				sum1=die1+die2;
-					
+				printf("YOUR STARTING POINT IS:%d\n",sum1);
 				if(sum1==7||sum1==11){
+					printf("YOUR POINT IS:%d\n",sum1);
 					printf("YOU WON :))\n");
 					break;
 				}
 				else if(sum1==2||sum1==3||sum1==12){
+					printf("YOUR POINT IS:%d\n",sum1);
 					printf("YOU LOST :((\n");
 					break;
 				}
@@ -30,33 +32,30 @@ int main(){
 									die1=rand()%6+1;
 									die2=rand()%6+1;
 									sum2=die1+die2;
-									if(sum2==sum1)
+									printf("YOUR POINT IS:%d\n",sum2);
+									if(sum2==sum1){
 										printf("YOU WON :))\n");
+										break;
+									}
 									else if(sum2==7){
 										printf("YOU LOST :((\n");
 										break;
 									}
 									sum2=0;
-										
 								}
 							else if(choice==0){
-								printf("EXITING...");
 								break;
 							}
 				        	else
-				            	 printf("WRONG CHOICE\n");
-					            
+				            	printf("WRONG CHOICE\n");
 			        }
-			        break;
 				}
 			case 0:
 				printf("EXITING...");
 			break;
 			default:
-			    printf("WRONG CHOICE\n");
+				printf("WRONG CHOICE");
 			break;
-			
 		}
-	}
 	return 0;
 }
