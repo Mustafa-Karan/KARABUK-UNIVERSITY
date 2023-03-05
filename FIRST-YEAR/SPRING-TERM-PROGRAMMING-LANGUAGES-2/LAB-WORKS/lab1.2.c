b5cad3cea9e401bf1d6909caa9635ddce7c1c3a3
Mustafa Karan <115796509+Mustafa-Karan@users.noreply.github.com>
2023-03-05 14:02:10 +0300

    Add files via upload

---
+++ FIRST-YEAR/SPRING-TERM-PROGRAMMING-LANGUAGES-2/LAB-WORKS/lab1.2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int bodyIndex(float,float);
+int main(){
+	float weight,height,result;
+	printf("**** WELCOME TO BODY INDEX CALCULATOR ****\n");
+	printf("ENTER YOUR WEIGHT:");
+	scanf("%f",&weight);
+	printf("ENTER YOUR HEIGHT:");
+	scanf("%f",&height);
+	result=bodyIndex(weight,height);
+	switch(result){
+		case 1:
+			printf("THIN");
+			break;
+		case 2:
+			printf("NORMAL");
+			break;
+		case 3:
+			printf("FAT");
+			break;
+		case 4:
+			printf("OBESE");
+			break;
+	}
+	return 0;
+}
+int bodyIndex(float w,float h){
+	float BMI;
+	BMI=w/(h*h);
+	if(BMI>=0&&BMI<19)
+	return 1;
+	else if(BMI>=19&&BMI<26)
+	return 2;
+	else if(BMI>=26&&BMI<30)
+	return 3;
+	else
+	return 4;
+}
