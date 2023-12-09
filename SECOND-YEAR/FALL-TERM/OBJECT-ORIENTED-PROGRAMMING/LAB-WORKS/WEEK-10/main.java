e80a1657ebca710cc15a0ae76e5e03ccc5614965
Mustafa Karan <115796509+Mustafa-Karan@users.noreply.github.com>
2023-12-09 20:19:18 +0300

    main.java

--- SECOND-YEAR/FALL-TERM/OBJECT-ORIENTED-PROGRAMMING/LAB-WORKS/WEEK-10/main.java
+++ SECOND-YEAR/FALL-TERM/OBJECT-ORIENTED-PROGRAMMING/LAB-WORKS/WEEK-10/main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
 public class main {
-    public static void main(String[] args){//2 3 6
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         Boolean b= true;
         double radius,height,width;
